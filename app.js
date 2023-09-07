ed127052b1e8fd96b0fb5a4805bb3379110a7f33
oxo404tk <141526133+oxo404tk@users.noreply.github.com>
2023-09-07 18:38:28 +0100

    Update new work on app.js 📑🚀

--- app.js
+++ app.js
@@ -10,50 +10,70 @@
 
 const lastWorkLayout = 
 `
-    <a class="GSBot">
-        <span class="name"><i class='bx bxs-bot'></i>Google Search Bot</span>
+    <a class="GSBot" href="https://t.me/GoogleYourTextToSpeechBot" >
+        <span class="name"><i class='bx bxs-bot'></i>Google Text To Speech Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="GTBot">
+    <a class="GTBot" href="https://t.me/GoogleTranslatebyBot">
         <span class="name"><i class='bx bxs-bot'></i>Google Translate Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
-    <a class="GBBot" >
+    <a class="GBBot" href="https://t.me/BardAIChatWithBot">
         <span class="name"><i class='bx bxs-bot'></i>Google Bard AI Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="RBBot" >
+    <a class="RBBot" href="https://t.me/RemoveYourBackgroundImageBot">
         <span class="name"><i class='bx bxs-bot'></i>Remove Background Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="SLBot" >
+    <a class="SLBot" href="https://t.me/shortYourLinkByBot">
         <span class="name"><i class='bx bxs-bot'></i>Shorten Link Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="ITBot" >
+    <a class="ITBot" href="https://t.me/GetLocationWithPhoneNumberBot">
+        <span class="name"><i class='bx bxs-bot'></i>Get Phone Number Location Bot</span>
+        <span class="ico"><i class='bx bx-link-external'></i></span>
+    </a>
+
+     <a class="ITBot" href="https://t.me/DailyWIMBot">
+        <span class="name"><i class='bx bxs-bot'></i>Daily WIM Bot</span>
+        <span class="ico"><i class='bx bx-link-external'></i></span>
+    </a>
+    
+    <a class="ADJMBot" href="https://t.me/setavatarBot">
+        <span class="name"><i class='bx bxs-bot'></i>Set Avatar Bot</span>
+        <span class="ico"><i class='bx bx-link-external'></i></span>
+    </a>
+
+    <a class="ADJMBot" href="https://t.me/YuGiOhCardStoreBot">
+        <span class="name"><i class='bx bxs-bot'></i>Yu-Gi-Oh ! Card Store Bot</span>
+        <span class="ico"><i class='bx bx-link-external'></i></span>
+    </a>
+
+     <a class="ITBot" href="https://t.me/byoxo404tk">
         <span class="name"><i class='bx bxs-bot'></i>IP Tracker Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="WIBot" >
+    <a class="WIBot" href="https://t.me/byoxo404tk">
         <span class="name"><i class='bx bxs-bot'></i>Who Is Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
 
-    <a class="FGBot" >
+    <a class="FGBot" href="https://t.me/byoxo404tk">
         <span class="name"><i class='bx bxs-bot'></i>Favicon Generator Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
-
-    <a class="ADJMBot" >
+    
+    <a class="ADJMBot" href="https://t.me/byoxo404tk">
         <span class="name"><i class='bx bxs-bot'></i>Auto Delete Join Message Bot</span>
         <span class="ico"><i class='bx bx-link-external'></i></span>
     </a>
-        
+
 `
 
 const body = document.querySelector("body")
